e7019ab709585b6359a8f51e0bdf17d91357e45a
Jan Bílek <jan@localazy.com>
2024-04-16 14:49:21 +0200

    Initial Commit

---
+++ ios-demo-new/ios_demo_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_demo_newApp.swift
+//  ios-demo-new
+//
+//  Created by Jan Bilek on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_demo_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
