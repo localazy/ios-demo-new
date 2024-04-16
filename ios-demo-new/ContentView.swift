2655eb7dca6f956c090b82ca11e006d409e138c5
Jan Bílek <jan@localazy.com>
2024-04-16 21:04:49 +0200

    Add images and styles

--- ios-demo-new/ContentView.swift
+++ ios-demo-new/ContentView.swift
@@ -10,29 +10,45 @@ struct ContentView: View {
     }
     
     var body: some View {
-        
         NavigationView {
-            List {
-                if let todaysSession = todaysSession {
-                    Section(header: Text("Today's session")) {
-                        NavigationLink(destination: SessionDetailView(session: todaysSession)) {
-                            Text(todaysSession.name)
+            ZStack(alignment: .top) {                
+                GeometryReader { geometry in
+                    Image("Background")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: geometry.size.width, height: geometry.safeAreaInsets.top + 200)
+                        .clipped()
+                        .overlay(
+                            LinearGradient(gradient: Gradient(colors: [.clear, .white]), startPoint: .top, endPoint: .bottom)
+                        )
+                }.frame(height: 200) // Fixed height for the image area
+
+                // List
+                List {
+                    if let todaysSession = todaysSession {
+                        Section(header: Text("Today's session")) {
+                            NavigationLink(destination: SessionDetailView(session: todaysSession)) {
+                                Text(todaysSession.name)
+                            }
+                        }
+                    } else {
+                        Section(header: Text("Today's session")) {
+                            Text("No session today")
                         }
                     }
-                } else {
-                    Section(header: Text("Today's session")) {
-                        Text("No session today")
-                    }
-                }
-                
-                Section(header: Text("All Sessions")) {
-                    ForEach(sessions.dropFirst()) { session in
-                        NavigationLink(destination: SessionDetailView(session: session)) {
-                            Text(session.name)
+
+                    Section(header: Text("All Sessions")) {
+                        ForEach(sessions.dropFirst()) { session in
+                            NavigationLink(destination: SessionDetailView(session: session)) {
+                                Text(session.name)
+                            }
                         }
                     }
                 }
+                .padding(.top, 200) 
+
             }
+            .edgesIgnoringSafeArea(.top)
             .navigationTitle("Meditation")
         }
     }
